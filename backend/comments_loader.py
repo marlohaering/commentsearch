96c916cdf05bd198efe971291c4b81a5401560b6
Tim Pietz <tim@pietz.me>
2020-10-13 21:05:33 +0200

    Add comments for database initialization

---
+++ backend/comments_loader.py
@@ -0,0 +1,41 @@
+import csv
+from itertools import islice
+
+from datasets import tqdm
+from pymongo import MongoClient
+
+from config import COMMENTS_FILE
+
+"""Create the comments.csv for initialization"""
+
+
+def generate_comments_file():
+    with COMMENTS_FILE.open('w+', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+
+        num = 100_000
+        for comment in tqdm(islice(fetch_root_comments_from_mongo(), num), total=num):
+            writer.writerow((comment,))
+
+
+def fetch_root_comments_from_mongo():
+    client = MongoClient('localhost', 27017)
+    comments = client['spon']['threads'].aggregate([
+        { '$unwind': { 'path': '$posts' } },
+        { '$match': { 'posts.body.0.type': { '$eq': 'content' } } },
+        { '$project': { '_id': 0, 'title': '$posts.title', 'body': { '$arrayElemAt': [ '$posts.body.text', 0 ] } } }
+    ])
+
+    for comment in comments:
+        title, body = comment['title'], comment['body']
+        if not body:
+            continue
+
+        if body[0].islower() and title:
+            yield f'{title} {body}'
+        else:
+            yield body
+
+
+if __name__ == '__main__':
+    generate_comments_file()

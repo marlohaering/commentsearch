2c5764c190deac860f89a7792bd8759cde75ed15
Marlo Häring <haering@informatik.uni-hamburg.de>
2020-10-14 12:54:36 +0200

    sessions

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,14 @@
 import csv
+import pickle
 
+import numpy as np
 import psycopg2
+from more_itertools import chunked, collapse
 from psycopg2.extras import execute_values
-from more_itertools import chunked
 from tqdm import tqdm
 
 from config import COMMENTS_FILE
+from embedding import get_embedding_for_texts
 
 
 def db_connect():
@@ -40,9 +43,10 @@ def init_schema(conn):
     cur = conn.cursor()
     cur.execute("DROP TABLE IF EXISTS documents")
     cur.execute(
-        "CREATE TABLE documents ("
-        "   id bigserial primary key,"
-        "   body text not null"
+        "CREATE TABLE documents ( "
+        "   id bigserial primary key, "
+        "   body text not null, "
+        "   embedding BYTEA not null"
         ")"
     )
 
@@ -52,9 +56,17 @@ def init_data(conn):
     cur = conn.cursor()
 
     with COMMENTS_FILE.open('r', encoding='utf-8') as f:
-        for comment_batch in chunked(tqdm(csv.reader(f)), n=500):
-            execute_values(cur, "INSERT INTO documents (body) VALUES %s", comment_batch)
+        for body_batch in chunked(tqdm(collapse(csv.reader(f))), n=10):
+            embedding_batch = get_embedding_for_texts(body_batch)
+            for body, embedding in zip(body_batch, embedding_batch):
+                print(body, embedding)
+                cur.execute("INSERT INTO documents (body, embedding) VALUES (%s, %s)", (body, pickle.dumps(embedding)))
 
+@with_connection
+def get_comment_embedding(conn, id: int):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM documents WHERE id = %s", (id,))
+    return cur.fetchone()
 
 if __name__ == '__main__':
     main()
--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,74 @@
-from typing import Optional
+from typing import Optional, DefaultDict, List
+from dataclasses import dataclass
+from functools import lru_cache, cached_property
+import numpy as np
+from embedding import get_embedding_for_texts
+import uvicorn
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from collections import defaultdict
 
-from fastapi import FastAPI
 
 app = FastAPI()
 
+class Session:
+    def __init__(self):
+        self.elements: List[ConceptElement] = []
+    
+    @property
+    def vector(self) -> np.ndarray:
+        return np.sum([e.embedding for e in self.elements], axis=0)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+sessions: DefaultDict[str, Session] = defaultdict(Session)
+
+def get_session(session_id: str) -> Session:
+    return sessions[session_id]
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+class ConceptElement(BaseModel):
+    pass
+
+    @property
+    def embedding(self):
+        raise NotImplementedError()
+
+class SeedTextParams(ConceptElement):
+    text: str
+
+    @cached_property
+    def embedding(self):
+        return get_embedding_for_texts([self.text])[0]
+
+
+class CommentAnnotationParams(ConceptElement):
+    id: int
+    positive: bool
+
+    @property
+    def parity(self) -> int:
+        return +1 if self.positive else -1
+
+    @property
+    def embedding(self):
+        return self.parity * get_comment_embedding(self.id)
+
+
+@app.post("/{session_id}/texts")
+def post_seed_text(params: SeedTextParams, session = Depends(get_session)):
+    session.elements.append(params)
+    print(session)
+    return 200
+
+
+@app.post("/{session_id}/comments")
+def post_comment_annotation(params: CommentAnnotationParams, session = Depends(get_session)):
+    session.elements.append(params)
+    print(session)
+    return 200
+
+
+# pip install torch torchvision transformers sentence-transformers
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8081, reload=True)

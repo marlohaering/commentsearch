5761551e6be65788d1b80f3f00a899f84af7eb8e
Marlo Häring <haering@informatik.uni-hamburg.de>
2020-10-16 16:38:58 +0200

    switch to sqlite

--- backend/commentsearch/config.py
+++ backend/commentsearch/config.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 
+SELECTED_DATA = 'whatsapp'
+
 WORK_DIR = Path.cwd() / 'work'
-COMMENTS_FILE = WORK_DIR / 'comments.csv'
-INDEX_FILE = lambda space: WORK_DIR / f'ann_index_{space}'
+CSV_SPON_COMMENTS = WORK_DIR / 'comments_spon.csv'
+CSV_WHATSAPP_COMMENTS = WORK_DIR / 'comments_whatsapp.csv'
+INDEX_FILE = lambda space: WORK_DIR / f'ann_index_{SELECTED_DATA}_{space}'
+SQLITE_SPON_COMMENTS = WORK_DIR / 'spon_comments.sqlite'
+SQLITE_WHATSAPP_COMMENTS = WORK_DIR / 'whatsapp_comments.sqlite'
--- backend/commentsearch/database.py
+++ backend/commentsearch/database.py
@@ -1,5 +1,6 @@
 import csv
 import pickle
+import sqlite3
 from functools import lru_cache
 from typing import List, Tuple
 
@@ -9,12 +10,12 @@ from more_itertools import chunked, collapse
 from psycopg2.extras import execute_values
 from tqdm import tqdm
 
-from commentsearch.config import COMMENTS_FILE
+from commentsearch.config import SQLITE_WHATSAPP_COMMENTS, CSV_WHATSAPP_COMMENTS
 from commentsearch.embedding import get_embedding_for_texts
 
 
 def db_connect():
-    return psycopg2.connect(host='localhost', port=5432, dbname='search', user='postgres', password='pw')
+    return sqlite3.connect(SQLITE_WHATSAPP_COMMENTS)
 
 
 def with_connection(fn):
@@ -45,10 +46,10 @@ def init_schema(conn):
     cur = conn.cursor()
     cur.execute("DROP TABLE IF EXISTS documents")
     cur.execute(
-        "CREATE TABLE documents ( "
-        "   id bigserial primary key, "
-        "   body text not null, "
-        "   embedding BYTEA not null"
+        "CREATE TABLE documents ("
+        "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+        "body TEXT, "
+        "embedding BLOB"
         ")"
     )
 
@@ -57,10 +58,10 @@ def init_schema(conn):
 def init_data(conn):
     cur = conn.cursor()
 
-    with COMMENTS_FILE.open('r', encoding='utf-8') as f:
+    with CSV_WHATSAPP_COMMENTS.open('r', encoding='utf-8') as f:
         for body_batch in chunked(tqdm(collapse(csv.reader(f))), n=500):
             embedding_batch = get_embedding_for_texts(body_batch)
-            execute_values(cur, "INSERT INTO documents (body, embedding) VALUES %s", zip(
+            conn.executemany("INSERT INTO documents (body, embedding) VALUES (?, ?)", zip(
                 body_batch, [pickle.dumps(e) for e in embedding_batch]))
 
 
@@ -69,14 +70,14 @@ def init_data(conn):
 def get_comment_embedding(conn, id: int) -> np.ndarray:
     print(f'Looking up embedding from db for {id}')
     cur = conn.cursor()
-    cur.execute("SELECT embedding FROM documents WHERE id = %s", (id,))
+    cur.execute("SELECT embedding FROM documents WHERE id = ?", (id,))
     return pickle.loads(cur.fetchone()[0])
 
 
 @with_connection
 def get_comment_body(conn, id: int) -> str:
     cur = conn.cursor()
-    cur.execute("SELECT body FROM documents WHERE id = %s", (id,))
+    cur.execute("SELECT body FROM documents WHERE id = ?", (id,))
     return cur.fetchone()[0]
 
 

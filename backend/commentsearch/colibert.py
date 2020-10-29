94b22d97aa6123b2f887f2e2c863e0d9c6e29549
Tim Pietz <tim@pietz.me>
2020-10-29 18:16:25 +0100

    Use GPU for colibert inference; Apply in mini batches

--- backend/commentsearch/colibert.py
+++ backend/commentsearch/colibert.py
@@ -1,24 +1,33 @@
 import itertools
 from typing import Union, List
+
 import torch
+from more_itertools import chunked
 from torch.nn import functional as F
 
 import numpy as np
+from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, PreTrainedTokenizerFast, \
     PreTrainedModel
+
+from commentsearch.config import COLIBERT_BATCH_SIZE
 
 
 class TextPairClassificationModel:
     def __init__(self, model_name: str):
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
         self.tokenizer: PreTrainedTokenizerFast = AutoTokenizer.from_pretrained(model_name, use_fast=True)
         self.model: PreTrainedModel = AutoModelForSequenceClassification.from_pretrained(model_name)
+        self.model.to(self.device)
 
     def get_pairwise_scores(self, texts_a: List[str], texts_b: List[str]) -> np.ndarray:
         text_pairs = itertools.product(texts_a, texts_b)
-        tokens = self.tokenizer(*list(zip(*text_pairs)), padding=True, truncation='longest_first', max_length=128, return_tensors='pt')
-        outputs, = self.model(**tokens)
-        preds = F.softmax(outputs, dim=-1)
-        return torch.reshape(preds[:, 1], (len(texts_a), len(texts_b))).detach().numpy()
+        batch_preds = []
+        for pair_batch in chunked(tqdm(text_pairs, total=len(texts_a) * len(texts_b)), n=COLIBERT_BATCH_SIZE):
+            tokens = self.tokenizer(*list(zip(*pair_batch)), padding=True, truncation='longest_first', max_length=128, return_tensors='pt').to(self.device)
+            outputs, = self.model(**tokens)
+            batch_preds.append(F.softmax(outputs.cpu(), dim=-1)[:, 1].detach().numpy())
+        return np.concatenate(batch_preds).reshape((len(texts_a), len(texts_b)))
 
 
 colibert: Union[None, TextPairClassificationModel] = None
--- backend/commentsearch/config.py
+++ backend/commentsearch/config.py
@@ -6,3 +6,5 @@ WORK_DIR = Path.cwd() / 'work'
 COMMENTS_CSV_FILE = WORK_DIR / f'comments_{SELECTED_DATA}.csv'
 INDEX_FILE = lambda space: WORK_DIR / f'ann_index_{SELECTED_DATA}_{space}'
 SQLITE_FILE = WORK_DIR / f'{SELECTED_DATA}_comments.sqlite'
+
+COLIBERT_BATCH_SIZE = 32
